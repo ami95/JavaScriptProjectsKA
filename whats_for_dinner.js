4217b805ba6361c37855ece02ab4e2680c8076ad
ami95 <mihaiadrian1995@gmail.com>
2019-06-04 09:21:11 +0200

    Create whats_for_dinner.js
    Drawing a pizza slice using JS

---
+++ whats_for_dinner.js
@@ -0,0 +1,20 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+//pizza slice
+fill(232, 138, 30);
+strokeWeight(5);
+stroke(64, 19, 19);
+arc(61,150,538,496,0,45);
+line(60,151,250,325);
+line(329,149,60,149);
+
+fill(255, 0, 0);
+ellipse(200, 200, 30, 30);
+ellipse(273, 181, 30, 30);
+
+fill(107, 30, 21);
+ellipse(241, 215, 30, 30);
+ellipse(147, 181, 30, 30);
+ellipse(226, 256, 30, 30);
